ff390a3bc174acd34d47066133a1f4760234c04a
ornella.petrelli <ornella.petrelli@gmail.com>
2024-12-22 22:19:24 -0300

    cambio status controller

--- src/controllers/statusController.js
+++ src/controllers/statusController.js
@@ -1,29 +1,28 @@
-import ResponseBuilder from "../utils/builders/responseBuilder.js"
-
 export const getPingController = (req, res) => {
-    
-    try{
-        console.log(req.user)
+    try {
+        if (!req.user) {
+            throw new Error('User not authenticated');
+        }
+        console.log(req.user);
         const response = new ResponseBuilder()
-        .setOk(true)
-        .setStatus(200)
-        .setMessage('Success')
-        .setPayload({
-            message: 'Pong'
-        })
-        .build()
-        
-        res.status(200).json(response)
-    }
-    catch(error){
+            .setOk(true)
+            .setStatus(200)
+            .setMessage('Success')
+            .setPayload({
+                message: 'Pong'
+            })
+            .build();
+
+        res.status(200).json(response);
+    } catch (error) {
         const response = new ResponseBuilder()
-        .setOk(false)
-        .setStatus(500)
-        .setMessage('Internal server error')
-        .setPayload({
-            detail: error.message
-        })
-        .build()
-        res.status(500).json(response)
+            .setOk(false)
+            .setStatus(500)
+            .setMessage('Internal server error')
+            .setPayload({
+                detail: error.message
+            })
+            .build();
+        res.status(500).json(response);
     }
-}
+};
